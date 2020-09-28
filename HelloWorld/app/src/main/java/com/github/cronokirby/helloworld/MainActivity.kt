76dcdc85319dd9ae50acc004a7b85cdde553c54b
Lúcás Meier <cronokirby@gmail.com>
2020-09-29 01:04:08 +0200

    Don't find DiceImage each time

--- HelloWorld/app/src/main/java/com/github/cronokirby/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/github/cronokirby/helloworld/MainActivity.kt
@@ -6,19 +6,20 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-        val randomInt = (1..6).random()
-
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-        val drawableResource = when (randomInt) {
+        val drawableResource = when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
