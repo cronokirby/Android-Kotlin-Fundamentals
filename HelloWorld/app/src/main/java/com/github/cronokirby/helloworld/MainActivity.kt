5e3738ff9adec0e69973f7a4e52fa39a1f5f4fc8
Lúcás Meier <cronokirby@gmail.com>
2020-09-29 00:28:02 +0200

    Add another button to increment roll

--- HelloWorld/app/src/main/java/com/github/cronokirby/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/github/cronokirby/helloworld/MainActivity.kt
@@ -13,11 +13,25 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
         val randomInt = (1..6).random()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val current = resultText.text.toString().toIntOrNull()
+        val next = when {
+            current == null -> 0
+            current >= 6 -> 6
+            else -> current + 1
+        }
+        resultText.text = next.toString()
     }
 }
