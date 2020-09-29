b74851aaac4ca680d1f46955d1ef06007e27374f
Lúcás Meier <cronokirby@gmail.com>
2020-09-29 18:33:21 +0200

    Style interactions in a much nicer way

--- AbouMe/app/src/main/java/com/github/cronokirby/aboutme/MainActivity.kt
+++ AbouMe/app/src/main/java/com/github/cronokirby/aboutme/MainActivity.kt
@@ -17,6 +17,21 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
         }
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickname(it)
+        }
+    }
+
+    private fun updateNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+        editText.requestFocus()
+        // Show the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
     }
 
     private fun addNickname(view: View) {
